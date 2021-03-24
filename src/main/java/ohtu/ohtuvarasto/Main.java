806e5e305d8fb4d6da9f65893731c64b273a88a8
Elina <kuukelo@gmail.com>
2021-03-24 22:13:03 +0200

    koodi virheellinen

--- src/main/java/ohtu/ohtuvarasto/Main.java
+++ src/main/java/ohtu/ohtuvarasto/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Varasto mehua = new Varasto(100.0);
-        Varasto olutta = new Varasto(100.0, 20.2);
+        Varasto olutta = new Varasto(100.0, 20.2, 5);
 
         System.out.println("Luonnin jälkeen:");
         System.out.println("Mehuvarasto: " + mehua);
